48e2d7da01fac57fc4813bae0a2f5dfe98dad92d
Phigros_1001301 <100998346+phigr1301@users.noreply.github.com>
2025-02-07 15:55:41 +0800

    fix bug

--- js/mod.js
+++ js/mod.js
@@ -77,7 +77,7 @@ function winText(){
 
 // If you add new functions anywhere inside of a layer, and those functions have an effect when called, add them here.
 // (The ones here are examples, all official functions are already taken care of)
-var doNotCallTheseFunctionsEveryTick = ["blowUpEverything"]
+var doNotCallTheseFunctionsEveryTick = ["blowUpEverything","uselsee_function"]
 
 function getStartPoints(){
     return new Decimal(modInfo.initialStartPoints)
